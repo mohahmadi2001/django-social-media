076185c8fabcaf4476a222d7b7602ffcaba00f84
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-15 19:18:22 +0330

    create UserRegistrationForm

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name","username", "email", "password", "password2")
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with this email address already exists.")
+        return email
