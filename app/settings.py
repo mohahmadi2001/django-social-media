8877ea34741dad868b390e559a3b5f3d9f1ac351
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-15 13:08:44 +0330

    start app

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    'accounts',
+    'blog',
+    'chats',
+    'friends',
+    'notification',
 ]
 
 MIDDLEWARE = [
