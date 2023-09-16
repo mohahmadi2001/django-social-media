383ab95b7bc20669a70ebbe78281040dc52885bb
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-16 11:05:21 +0330

    create UserRegisterView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from .forms import UserRegistrationForm
+from django.contrib import messages
 # Create your views here.
+
+class UserRegisterView(View):
+    template_name = 'users/register.html'
+    def post(self, request, *args, **kwargs):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request,f"Your account has been created! You can login now {username}")
+            return redirect("login")
+        context = {"form":form}
+        return render(
+            request=request,
+            template_name=self.template_name,
+            context=context
+        )
+            
